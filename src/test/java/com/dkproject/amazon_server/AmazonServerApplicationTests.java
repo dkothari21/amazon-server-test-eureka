d4c49d411547ce59f91785a7b539b7ec942741e2
Dip kothari <dipkothari@Dips-Laptop.lan>
2024-06-13 21:51:33 -0400

    Eureka amazon server test

---
+++ src/test/java/com/dkproject/amazon_server/AmazonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkproject.amazon_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
